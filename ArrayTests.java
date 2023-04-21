b47f65625bee29df374f6ade61afb2b6f3524361
holden <holden.pipp@gmail.com>
2023-04-20 19:07:51 -0700

    ArrayTests.java and ArrayExamples.java debug

--- ArrayTests.java
+++ ArrayTests.java
@@ -6,13 +6,18 @@ public class ArrayTests {
 	public void testReverseInPlace() {
     int[] input1 = { 3 };
     ArrayExamples.reverseInPlace(input1);
+    int[] input2 = { 1, 2, 3, 4 };
+    ArrayExamples.reverseInPlace(input2);
     assertArrayEquals(new int[]{ 3 }, input1);
+    assertArrayEquals(new int[]{ 4, 3, 2, 1 }, input2);
 	}
 
 
   @Test
   public void testReversed() {
     int[] input1 = { };
+    int[] input2 = { 1, 2, 3, 4 };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+    assertArrayEquals(new int[]{ 4, 3, 2, 1 }, ArrayExamples.reversed(input2));
   }
 }
